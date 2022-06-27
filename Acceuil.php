bd2e0acf63e7faa63e5b8e15fbda934e31695463
hamzat <hamzat1.siby@epitech.eu>
2022-06-27 18:39:43 +0200

    add fouter and logo twitter acceuil pages

---
+++ Acceuil.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="mini.twitter.ico">
+    <link href="acceuil.css" rel="stylesheet">
+
+    <title>Twitter</title>
+</head>
+<body>
+    <div class="blue">
+        <img src="./Untitled.png" alt="" ></img>
+    </div>
+    <header>
+        <h1>Ca se passe maintenant</h1>
+        <h3>Rejoignez Twitter dès aujourd'hui.</h3>
+    </header>
+    <div class="option">
+        <button value="S'inscrire avec Google">rrr</button>
+        <input type="submit" value="S'inscrire avec Apple">
+    
+        <div class="trai"></div><p>ou</p><div class="trai"></div>
+        <input type="submit" value="S'inscrire avec un numéro de teleph...">
+        <span>En vous inscrivant, vous acceptez les Conditions d'Utilisation et la Politique de Confidentialité, incluant l'Utilisation de Cookies.</span>
+    </div>
+    <div></div>
+
+    <footer>
+        <ul>
+            <li>À propos</li>
+            <li>Centre d'assistance</li>
+            <li>Centre d'assistance</li>
+            <li>Conditions d'utilisation</li>
+            <li>Politique de Confidentialité</li>
+            <li>Politique relative aux cookies</li>
+            <li>Accessibilité</li>
+            <li>Informations sur les publicités</li>
+            <li>Blog</li>
+            <li>Statut</li>
+            <li>Carrières</li>
+            <li>Ressources de la marque</li>
+            <li>Publicité</li>
+            <li>Marketing</li>
+        </ul>
+        <ul>
+            <li>Twitter pour les professionnels</li>
+            <li>Développeurs</li>
+            <li>Répertoire</li>
+            <li>Paramètres</li>
+            <li>© 2022 Twitter, Inc.</li>
+        </ul>
+    </footer>
+</body>
+</html>
