f4b89d68fabfaecd2596e504511159be751cb586
hamzat <hamzat1.siby@epitech.eu>
2022-06-28 19:26:39 +0200

    pages acceuil terminer + css add

--- Acceuil.php
+++ Acceuil.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="FR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,48 +9,62 @@
 
     <title>Twitter</title>
 </head>
-<body>
-    <div class="blue">
-        <img src="./Untitled.png" alt="" ></img>
-    </div>
-    <header>
-        <h1>Ca se passe maintenant</h1>
-        <h3>Rejoignez Twitter dès aujourd'hui.</h3>
-    </header>
-    <div class="option">
-        <button value="S'inscrire avec Google">rrr</button>
-        <input type="submit" value="S'inscrire avec Apple">
-    
-        <div class="trai"></div><p>ou</p><div class="trai"></div>
-        <input type="submit" value="S'inscrire avec un numéro de teleph...">
-        <span>En vous inscrivant, vous acceptez les Conditions d'Utilisation et la Politique de Confidentialité, incluant l'Utilisation de Cookies.</span>
-    </div>
-    <div></div>
 
-    <footer>
-        <ul>
-            <li>À propos</li>
-            <li>Centre d'assistance</li>
-            <li>Centre d'assistance</li>
-            <li>Conditions d'utilisation</li>
-            <li>Politique de Confidentialité</li>
-            <li>Politique relative aux cookies</li>
-            <li>Accessibilité</li>
-            <li>Informations sur les publicités</li>
-            <li>Blog</li>
-            <li>Statut</li>
-            <li>Carrières</li>
-            <li>Ressources de la marque</li>
-            <li>Publicité</li>
-            <li>Marketing</li>
-        </ul>
-        <ul>
-            <li>Twitter pour les professionnels</li>
-            <li>Développeurs</li>
-            <li>Répertoire</li>
-            <li>Paramètres</li>
-            <li>© 2022 Twitter, Inc.</li>
-        </ul>
-    </footer>
+<body>
+    <div class="app">
+        <div class="blue">
+            <img class="logo-blanc" src="./Untitled.png" alt=""></img>
+        </div>
+        <header>
+            <img class="logo-bleu" src="./logotwitter.png" alt></img>
+            <h1>Ca se passe<br> maintenant</h1><br><br>
+            <h3>Rejoignez Twitter dès aujourd'hui.</h3>
+        </header>
+        <div class="option">
+            <input type="submit" value="S'inscrire avec Google" class="bt">
+            <input type="submit" value="S'inscrire avec Apple" class="bt1">
+
+            <div class="trai">
+                <p>ou</p>
+            </div>
+            <div class="tel">
+                <input class="bt3" type="submit" value="S'inscrire avec un numéro de teleph...">
+                <span>
+                    En vous inscrivant, vous acceptez les Conditions d'Utilisation<br> et la Politique de Confidentialité, incluant l'Utilisation de<br> Cookies.
+                </span>
+            </div>
+        </div>
+        <div class="connect">
+            <h4>Vous avez déjà un compte ?</h4><br>
+            <input class="bt4" type="submit" value="S'inscrire avec Apple">
+        </div>
+
+        <footer>
+            <ul>
+                <li>À propos</li>
+                <li>Centre d'assistance</li>
+                <li>Centre d'assistance</li>
+                <li>Conditions d'utilisation</li>
+                <li>Politique de Confidentialité</li>
+                <li>Politique relative aux cookies</li>
+                <li>Accessibilité</li>
+                <li>Informations sur les publicités</li>
+                <li>Blog</li>
+                <li>Statut</li>
+                <li>Carrières</li>
+                <li>Ressources de la marque</li>
+                <li>Publicité</li>
+                <li>Marketing</li>
+            </ul>
+            <ul>
+                <li>Twitter pour les professionnels</li>
+                <li>Développeurs</li>
+                <li>Répertoire</li>
+                <li>Paramètres</li>
+                <li>© 2022 Twitter, Inc.</li>
+            </ul>
+        </footer>
+    </div>
 </body>
+
 </html>
