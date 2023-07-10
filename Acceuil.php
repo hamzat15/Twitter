43f8f3615c0eabfaa11f2f284b52e2f6463e8f9a
PC-H <PC_H@PC-H>
2023-07-10 14:08:04 +0200

    refonte du site css et html

--- Acceuil.php
+++ Acceuil.php
@@ -1,78 +1,62 @@
 <!DOCTYPE html>
 <html lang="FR">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="mini.twitter.ico">
     <link href="acceuil.css" rel="stylesheet">
-
     <title>Twitter</title>
+    <link rel="short icon" href="./assets/logotwitter.png"/> 
 </head>
-
 <body>
-    <div class="app">
-        <div class="blue">
-            <img class="logo-blanc" src="./assets/Untitled.png" alt=""></img>
+    <div class="main">
+        <div class="leftside">
+            <img src="./assets/Untitled.png" alt="" srcset="">
         </div>
-        <header>
-            <img class="logo-bleu" src="./assets/logotwitter.png" alt></img>
-            <h1>Ca se passe<br> maintenant</h1><br><br>
-            <h3>Rejoignez Twitter dès aujourd'hui.</h3>
-        </header>
-        <div class="option">
-            <input type="submit" value="S'inscrire avec Google" class="bt">
-            <input type="submit" value="S'inscrire avec Apple" class="bt1">
-            
-            <div class="trai">
-                <p>ou</p>
+        <div class="rightside">
+            <img class="logo-bleu" src="./assets/logotwitter.png" alt></img> 
+            <div class="header">
+                <h1>Ca se passe<br> maintenant</h1><br>
             </div>
-            <div class="tel">
-                <input class="bt3" type="submit" value="S'inscrire avec un numéro de teleph...">
-                <span>
-                    En vous inscrivant, vous acceptez les Conditions d'Utilisation<br> et la Politique de Confidentialité, incluant l'Utilisation de<br> Cookies.
-                </span>
+           
+            <div class="option">
+                <h3>Rejoignez Twitter dès aujourd'hui.</h3>
+                <div class="btnSign">
+                    <div class="google">
+                        <input type="submit" value="S'inscrire avec Google" class="">
+                    </div>
+                    <div class="apple">
+                        <input type="submit" value="S'inscrire avec Apple" class="">
+                    </div>
+                </div>
+                <div class="ou">
+                    <p>ou</p>
+                </div>
+                <div class="btnTel">
+                    <input type="submit" value="Creer un compte">
+                </div>
+                <div class="spanSign">
+                    <span>
+                        En vous inscrivant, vous acceptez les Conditions<br> d'Utilisation et la Politique de Confidentialité, incluant<br> l'Utilisation de Cookies.
+                    </span>
+                </div>
             </div>
+            <div class="connect">
+                <h4>Vous avez déjà un compte ?</h4><br>
+                <input onClick="AfficherMasquer()" type="button" value="Se connecter" >
+            </div>
+            <!--
+            <div id="divacacher" style="display:none;">
+                <input class="cross" onClick="delet()" type="button" value="X">
+                <input type="text" id="name" name="name" placeholder="password" require>
+                <input type="text" id="password" name="password" placeholder="password" require>
+                <a href="./register/register.php">Accueil</a>
+            </div> -->  
         </div>
-        <div class="connect">
-            <h4>Vous avez déjà un compte ?</h4><br>
-            <input class="bt4" onClick="AfficherMasquer()" type="button" value="Se connecter" >
-        </div>
-        <div id="divacacher" style="display:none;">
-            <input class="cross" onClick="delet()" type="button" value="X">
-            <input type="text" id="name" name="name" placeholder="password" require>
-            <input type="text" id="password" name="password" placeholder="password" require>
-            <a href="./register/register.php">Accueil</a>
-
-        </div>
-
-        <footer>
-            <ul>
-                <li>À propos</li>
-                <li>Centre d'assistance</li>
-                <li>Centre d'assistance</li>
-                <li>Conditions d'utilisation</li>
-                <li>Politique de Confidentialité</li>
-                <li>Politique relative aux cookies</li>
-                <li>Accessibilité</li>
-                <li>Informations sur les publicités</li>
-                <li>Blog</li>
-                <li>Statut</li>
-                <li>Carrières</li>
-                <li>Ressources de la marque</li>
-                <li>Publicité</li>
-                <li>Marketing</li>
-            </ul>
-            <ul>
-                <li>Twitter pour les professionnels</li>
-                <li>Développeurs</li>
-                <li>Répertoire</li>
-                <li>Paramètres</li>
-                <li>© 2022 Twitter, Inc.</li>
-            </ul>
-        </footer>
+    </div>     
+    <div class="footer">
+        <span class="span">00kngrliniotnùotihigùhoipjt0</span>
     </div>
     <script src="./index.js"></script>
-</body>
-
+    </body>
 </html>
