16389ef706f049632b41f698e90f192436ae20db
PC-H <PC_H@PC-H>
2023-07-11 11:22:12 +0200

    add modale connecte

--- Acceuil.php
+++ Acceuil.php
@@ -9,54 +9,70 @@
     <link rel="short icon" href="./assets/logotwitter.png"/> 
 </head>
 <body>
-    <div class="main">
-        <div class="leftside">
-            <img src="./assets/Untitled.png" alt="" srcset="">
-        </div>
-        <div class="rightside">
-            <img class="logo-bleu" src="./assets/logotwitter.png" alt></img> 
-            <div class="header">
-                <h1>Ca se passe<br> maintenant</h1><br>
+    <div class="connection" id="divcacher" style="display:none;"></div>
+        <div class="opt" id="opt"  style="display:none;"> 
+            <div class="delete">
+                <input class="del" onClick="delet()" type="button" value="X">
             </div>
-           
-            <div class="option">
-                <h3>Rejoignez Twitter dès aujourd'hui.</h3>
-                <div class="btnSign">
-                    <div class="google">
-                        <input type="submit" value="S'inscrire avec Google" class="">
+            <img class="logo-bleu" src="./assets/logotwitter.png" alt></img>  
+            <div class="left">
+                <h3>Connectez vous a<br> Twitter</h3>
+                <div class="btnT">
+                    <div>
+                        <input type="text" id="name" name="name" placeholder="email" require>
                     </div>
-                    <div class="apple">
-                        <input type="submit" value="S'inscrire avec Apple" class="">
+                    <div>
+                        <input type="text" id="password" name="password" placeholder="password" require>
                     </div>
                 </div>
-                <div class="ou">
-                    <p>ou</p>
+                <div class="btnConnect">
+                    <input  type="submit" value="Suivant">
                 </div>
-                <div class="btnTel">
-                    <input type="submit" value="Creer un compte">
-                </div>
-                <div class="spanSign">
-                    <span>
-                        En vous inscrivant, vous acceptez les Conditions<br> d'Utilisation et la Politique de Confidentialité, incluant<br> l'Utilisation de Cookies.
-                    </span>
+                <div>
+                    <p>Vous n'avez pas de compte ? <a href="">Inscrivez-vous</a></p>
                 </div>
             </div>
-            <div class="connect">
-                <h4>Vous avez déjà un compte ?</h4><br>
-                <input onClick="AfficherMasquer()" type="button" value="Se connecter" >
-            </div>
-            <!--
-            <div id="divacacher" style="display:none;">
-                <input class="cross" onClick="delet()" type="button" value="X">
-                <input type="text" id="name" name="name" placeholder="password" require>
-                <input type="text" id="password" name="password" placeholder="password" require>
-                <a href="./register/register.php">Accueil</a>
-            </div> -->  
-        </div>
-    </div>     
-    <div class="footer">
-        <span class="span">00kngrliniotnùotihigùhoipjt0</span>
     </div>
-    <script src="./index.js"></script>
+        <div class="main">
+            <div class="leftside">
+                <img src="./assets/Untitled.png" alt="" srcset="">
+            </div>
+            <div class="rightside">
+                <img class="logo-bleu" src="./assets/logotwitter.png" alt></img> 
+                <div class="header">
+                    <h1>Ca se passe<br> maintenant</h1><br>
+                </div>   
+                <div class="option">
+                    <h3>Rejoignez Twitter dès aujourd'hui.</h3>
+                    <div class="btnSign">
+                        <div>
+                            <input type="button" value="S'inscrire avec Google">
+                        </div>
+                        <div>
+                            <input type="submit" value="S'inscrire avec Apple">
+                        </div>
+                    </div>
+                    <div class="ou">
+                        ou
+                    </div>
+                    <div class="btnTel">
+                        <input type="submit" value="Creer un compte">
+                    </div>
+                    <div class="spanSign">
+                        <span>
+                            En vous inscrivant, vous acceptez les Conditions<br> d'Utilisation et la Politique de Confidentialité, incluant<br> l'Utilisation de Cookies.
+                        </span>
+                    </div>
+                </div>
+                <div class="connect">
+                    <h4>Vous avez déjà un compte ?</h4><br>
+                    <input onClick="AfficherMasquer()" type="button" value="Se connecter" >
+                </div>      
+            </div>
+        </div>     
+        <div class="footer">
+        <span class="span">00kngrliniotnùotihigùhoipjt0</span>
+        </div>
+        <script src="./index.js"></script>
     </body>
 </html>
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const AfficherMasquer = () => {
-  divInfo = document.getElementById("divacacher");
-
+  divInfo = document.getElementById("divcacher");
   if (divInfo.style.display == "none") divInfo.style.display = "block";
   else divInfo.style.display = "none";
-}
+
+   divInfo2 = document.getElementById("opt");
+   if (divInfo2.style.display == "none") divInfo2.style.display = "block";
+   else divInfo2.style.display = "none";
+
+   body = document.body;
+   body.style.overflow = "hidden";
+};
 AfficherMasquer();
 
 const delet = () => {
   if (divInfo.style.display == "none") divInfo.style.display = "block";
   else divInfo.style.display = "none";
+  if (divInfo2.style.display == "none") divInfo2.style.display = "block";
+  else divInfo2.style.display = "none";
+
+  body.style.removeProperty("overflow");
+
 };
 delet();
